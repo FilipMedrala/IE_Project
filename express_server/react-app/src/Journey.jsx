8b2d3da4871fab1e3eddf4d2edb58cb5e08a274c
Yzh-yyy <145213517+Yzh-yyy@users.noreply.github.com>
2024-04-06 00:46:42 +1100

    creacte journey section1 and question template and popup

--- express_server/react-app/src/Journey.jsx
+++ express_server/react-app/src/Journey.jsx
@@ -1,13 +1,106 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './FallingFood.css'
+// import { Carousel } from 'antd';
+//import { FullSlip, SlipItem } from "react-fullslip";
+import { Button, Popover } from 'antd';
+import pic1 from './assets/3.png'
+import pic2 from './assets/4.png'
+import pic3 from './assets/5.png'
+import pic4 from './assets/6.png'
+import pic5 from './assets/left-btn.png'
+import pic6 from './assets/right-btn.png'
+import { Carousel } from 'antd';
 
 
 export default function FallingFood() {
 
+
+  const myRef = useRef(null);
   return (
-    <div>
-     <div className="header">Journey</div>
-    
+    <div className="Journey">
+
+      {/* <div className="header">Journey</div> */}
+      {/* <FullSlip {...{
+        navigation: false,           //是否开启导航点,      默认为true
+        activeClass: 'active',      //自定义导航点类名,    默认为active, .navigation-dot下的.active
+        duration: 1000,              //屏幕滑动切换的时间,  默认为1000
+        transverse: false,            //是否更改为横向滑动,  默认为false
+        // navImage:[require('./assets/1.jpg'),require('./assets/2.jpg'),require('./assets/3.jpg')],   //导航点图片,可选,默认无图片
+        arrowNav: true,              //是否开启箭头导航     默认false不开启
+      }}> */}
+
+{/* <Carousel dotPosition={'right'}> */}
+        <div className="itembox itembox1" style={{ backgroundColor: '#C1FFC1' }}>
+          <div className="firstPageContWrap">
+            <div className="firstPageCont">
+              <div>
+                <Popover content={'People have developed several kinds of apples with different tastes.'} title="Title">
+                  <img className="icons icons0" src={pic1} alt="" />
+                </Popover>
+              </div>
+              <div>
+                <Popover content={'2222222222222222'} title="Title">
+                  <img className="icons icons1" src={pic2} alt="" />
+                </Popover>
+              </div>
+              <div>
+                <Popover content={'2222222222222222'} title="Title">
+                  <img className="icons icons2" src={pic3} alt="" />
+                </Popover>
+              </div>
+              <div>
+                <Popover content={'2222222222222222'} title="Title">
+                  <img className="icons icons3" src={pic4} alt="" />
+                </Popover>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="swiper">
+            <div className="swiperInner" >
+              <div className="leftIcon sameicon" onClick={() => { myRef?.current?.prev?.() }}>
+                <img src={pic5} alt="" />
+              </div>
+              <div className="rightIcon sameicon" onClick={() => { myRef?.current?.next?.() }}>
+                <img src={pic6} alt="" />
+              </div>
+              <Carousel dotPosition={'bottom'} ref={myRef}  >
+                <div>
+                  <div className="swiperItem" key={'1'}  >
+                    The plump, juicy blueberries glistened in the sunlight, their deep indigo hue a feast for the eyes.
+                    < br />As I bit into the ripe blueberry, a burst of sweet,
+                    < br />The plump, juicy blueberries glistened in the sunlight, their deep indigo hue a feast for the eyes.
+                    < br /> As I bit into the ripe blueberry, a burst of sweet,
+
+                  </div>
+                </div>
+                <div>
+                  <div className="swiperItem" key={'2'}  >222</div>
+                </div>
+                <div>
+                  <div className="swiperItem" key={'3'}  >333</div>
+                </div>
+                <div>
+                  <div className="swiperItem" key={'4'}  >444</div>
+                </div>
+              </Carousel>
+            </div>
+          </div>
+
+        </div>
+        <div className="itembox itembox2" style={{ backgroundColor: '#C6E2FF' }}>
+          page2
+        </div>
+        <div className="itembox itembox3" style={{ backgroundColor: '#FFEC8B' }}>
+          page3
+        </div>
+        <div className="itembox itembox4" style={{ backgroundColor: '#cccccc' }}>
+          page4
+        </div>
+      {/* </FullSlip> */}
+      {/* </Carousel> */}
+
     </div>
   );
 }
