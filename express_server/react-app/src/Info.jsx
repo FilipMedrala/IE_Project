80daea30431a84d131013f7ed711963d24d1d071
diamondor <113500123+diamondor@users.noreply.github.com>
2024-04-05 23:30:04 +1100

    Info Page
    4 important key statistics of obesity, fruit and veggie consumption, and sugary drink consumption

--- express_server/react-app/src/Info.jsx
+++ express_server/react-app/src/Info.jsx
@@ -1,11 +1,77 @@
-import React, { useState, useEffect } from "react";
-import './FallingFood.css'
-export default function FallingFood() {
+import React from 'react';
+import { Statistic } from 'antd';
+import { FaPerson } from "react-icons/fa6";
+import { GiFruitBowl, GiWrappedSweet, GiSodaCan } from "react-icons/gi";
+import './StatisticsCards.css'; // Import CSS file for styling
 
-  return (
-    <div>
-     <div className="header">Info</div>
-    
+const StatisticsCards = () => (
+  <div className="card-container">
+    {/* Statistic Cards Component */}
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front">
+          <Statistic
+            title="Childhood Obesity"
+            value={25}
+            suffix="%"
+            prefix={<FaPerson />}
+            valueStyle={{ fontSize: '34px' }}
+          />
+        </div>
+        <div className="card-back">
+          <p>25% overweight or obese (5-17 years)</p>
+        </div>
+      </div>
     </div>
-  );
-}
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front">
+          <Statistic
+            title="Healthy Eating Habits"
+            value={6}
+            suffix="%"
+            prefix={<GiFruitBowl />}
+            valueStyle={{ fontSize: '34px' }}
+          />
+        </div>
+        <div className="card-back">
+          <p>6% meet fruit and veg recommendations</p>
+        </div>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front">
+          <Statistic
+            title="Sugar Awareness"
+            value={9}
+            suffix="%"
+            prefix={<GiWrappedSweet />}
+            valueStyle={{ fontSize: '34px' }}
+          />
+        </div>
+        <div className="card-back">
+          <p>9% adults, 7% children drink sugary drinks daily</p>
+        </div>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front">
+          <Statistic
+            title="Drink Smart"
+            value={45}
+            suffix="%"
+            prefix={<GiSodaCan />}
+            valueStyle={{ fontSize: '34px' }}
+          />
+        </div>
+        <div className="card-back">
+          <p>45% children consume sugary or diet drinks weekly (2-17 years)</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default StatisticsCards;
