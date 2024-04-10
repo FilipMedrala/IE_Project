ba54b3dfbe0579068e558b091c3adb30830df1a9
FilipMedrala <62007669+FilipMedrala@users.noreply.github.com>
2024-04-10 17:41:37 +1000

    Landing page revamp

--- express_server/react-app/src/App.jsx
+++ express_server/react-app/src/App.jsx
@@ -1,60 +1,341 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Router, Link } from "react-router-dom";
-import Snake from "./Snake";
-import FallingFood from "./FallingFood";
-import NotFound from "./NotFound"
-import Home from "./Home";
-import Info from "./Info";
-import Journey from "./Journey";
-import './App.css'
-import web_logo from './assets/web_logo.jpg'
+import React, { useEffect } from "react";
+import family from "./assets/family.jpg";
+import game from "./assets/game.png";
+import play from "./assets/play.png";
+import pierogi from "./assets/pierogi.png";
+import background from "./assets/background.png";
 
 export default function App() {
+  useEffect(() => {
+    const scrollHandler = () => {
+      const scrollpos = window.scrollY;
+      const header = document.getElementById("header");
+      const navcontent = document.getElementById("nav-content");
+      const navaction = document.getElementById("navAction");
+      const toToggle = document.querySelectorAll(".toggleColour");
+
+      if (scrollpos > 10) {
+        header.classList.add("bg-white");
+        navaction.classList.remove("bg-white");
+        navaction.classList.add("gradient");
+        navaction.classList.remove("text-gray-800");
+        navaction.classList.add("text-white");
+        for (let i = 0; i < toToggle.length; i++) {
+          toToggle[i].classList.add("text-gray-800");
+          toToggle[i].classList.remove("text-white");
+        }
+        header.classList.add("shadow");
+        navcontent.classList.remove("bg-gray-100");
+        navcontent.classList.add("bg-white");
+      } else {
+        header.classList.remove("bg-white");
+        navaction.classList.remove("gradient");
+        navaction.classList.add("bg-white");
+        navaction.classList.remove("text-white");
+        navaction.classList.add("text-gray-800");
+        for (let i = 0; i < toToggle.length; i++) {
+          toToggle[i].classList.add("text-white");
+          toToggle[i].classList.remove("text-gray-800");
+        }
+        header.classList.remove("shadow");
+        navcontent.classList.remove("bg-white");
+        navcontent.classList.add("bg-gray-100");
+      }
+    };
+
+    const check = (e) => {
+      const target = (e && e.target) || (event && event.srcElement);
+      const navMenuDiv = document.getElementById("nav-content");
+      const navMenu = document.getElementById("nav-toggle");
+
+      if (!checkParent(target, navMenuDiv)) {
+        if (checkParent(target, navMenu)) {
+          if (navMenuDiv.classList.contains("hidden")) {
+            navMenuDiv.classList.remove("hidden");
+          } else {
+            navMenuDiv.classList.add("hidden");
+          }
+        } else {
+          navMenuDiv.classList.add("hidden");
+        }
+      }
+    };
+
+    const checkParent = (t, elm) => {
+      while (t.parentNode) {
+        if (t === elm) {
+          return true;
+        }
+        t = t.parentNode;
+      }
+      return false;
+    };
+
+    document.addEventListener("scroll", scrollHandler);
+    document.addEventListener("click", check);
+
+    return () => {
+      document.removeEventListener("scroll", scrollHandler);
+      document.removeEventListener("click", check);
+    };
+  }, []);
+
   return (
-    <div >
-      <BrowserRouter>
-        <div className="header">
-        <div className="logo">
-        <Link to="/">
-              <img src={web_logo} alt="Home"  />
-            </Link>
+    <>
+      <nav id="header" className="fixed w-full z-30 top-0 text-white">
+        <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+          <div className="pl-4 flex items-center">
+            <a
+              className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+              href="#"
+            >
+              <img
+                className="w-full md:w-1/12 z-50"
+                src={pierogi}
+                alt="Pierogi"
+              />
+              Healthy Journey
+            </a>
           </div>
-          
-          <ul>
-            <li>
-              <Link to="/info">Health Awareness</Link>
-            </li>
-          </ul>
-        
+          <div className="block lg:hidden pr-4">
+            <button
+              id="nav-toggle"
+              className="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+            >
+              <svg
+                className="fill-current h-6 w-6"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
+          <div
+            className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
+            id="nav-content"
+          >
+            <ul className="list-reset lg:flex justify-end flex-1 items-center">
+              <li className="mr-3">
+                <a
+                  className="inline-block py-2 px-4 text-black font-bold no-underline"
+                  href="#"
+                >
+                  Information
+                </a>
+              </li>
+              <li className="mr-3">
+                <a
+                  className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                  href="#"
+                >
+                  Games
+                </a>
+              </li>
+              <li className="mr-3">
+                <a
+                  className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                  href="#"
+                >
+                  Journey
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div  className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/info" element={<Info />} />
-          <Route path="/Journey" element={<Journey />} />
-          <Route path="/Snake" element={<Snake />} />
-          <Route path="/FallingFood" element={<FallingFood />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
+      </nav>
+      <div className="pt-24">
+        <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+          <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+            <p className="uppercase tracking-loose w-full">
+              Does your kid know what's healthy?
+            </p>
+            <h1 className="my-4 text-5xl font-bold leading-tight">
+              We can help you!
+            </h1>
+            <p className="leading-normal text-2xl mb-8">
+              Embark on a noble quest with your children to uncover the wonders
+              of healthy eating and why it matters! 🛡️🍏
+            </p>
+            <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+              Want to learn more?
+            </button>
+          </div>
+          <div className="w-full md:w-3/5 py-6 text-center">
+            <img className="w-full md:w-4/5 z-50" src={family} alt="Family" />
+          </div>
         </div>
-        <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 Protect Your Health™. All Rights Reserved.
-    </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">About</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline">Contact</a>
-        </li>
-    </ul>
-    </div>
-</footer>
-      </BrowserRouter>
-    </div>
+      </div>
+      {/* <div className="relative -mt-12 lg:-mt-24">
+        <svg viewBox="0 0 1428 174" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink ="http://www.w3.org/1999/xlink">
+          <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fillRule="nonzero">
+              <path d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496" opacity="0.100000001"></path>
+              <path d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z" opacity="0.100000001"></path>
+              <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" id="Path-4" opacity="0.200000003"></path>
+            </g>
+            <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fillRule="nonzero">
+              <path d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"></path>
+            </g>
+          </g>
+        </svg>
+      </div> */}
+      <section className="bg-white border-b py-8">
+        <div className="container max-w-5xl mx-auto m-8">
+          <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+            Welcome to Healthy Journey!
+          </h2>
+          <div className="w-full mb-4">
+            <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+          </div>
+          <div className="flex flex-wrap">
+            <div className="w-5/6 sm:w-1/2 p-6">
+              <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                Healthy eating habits in a more fun way! 🥕
+              </h3>
+              <p className="text-gray-600 mb-8">
+                In a landscape dominated by sugary snacks and beverages,
+                convincing children of the importance of nutrient-rich foods
+                remains an uphill battle for many families. Luckily, we have
+                just the thing to help you... GAMES!
+                <br />
+                <br />
+                <a
+                  className="text-pink-500 underline"
+                  href="https://undraw.co/"
+                >
+                  Find out more
+                </a>
+              </p>
+            </div>
+            <div className="w-full sm:w-1/2 p-6">
+              <img className="w-full md:w-4/5 z-50" src={play} alt="Play" />
+            </div>
+          </div>
+          <div className="flex flex-wrap flex-col-reverse sm:flex-row">
+            <div className="w-full sm:w-1/2 p-6 mt-6">
+              <img className="w-full md:w-4/5 z-50" src={game} alt="Game" />
+            </div>
+            <div className="w-full sm:w-1/2 p-6 mt-6">
+              <div className="align-middle">
+                <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                  Playing Journey: The Healthy Quest 🗡️
+                </h3>
+                <p className="text-gray-600 mb-8">
+                  Embark on an epic scrolling journey through a vibrant browser
+                  world where every click is a step towards healthier choices!
+                  🍎🥕 Along the way, your child will encounter a colorful array
+                  of tempting treats, both nutritious and not-so-nutritious. But
+                  fear not! With each encounter, they'll learn the invaluable
+                  skill of distinguishing between the two, empowering them to
+                  make informed decisions about what they eat.
+                  <br />
+                  <br />
+                  <a
+                    className="text-pink-500 underline"
+                    href="https://undraw.co/"
+                  >
+                    Find out more
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="bg-white border-b py-8">
+        <div className="container mx-auto flex flex-wrap pt-4 pb-12">
+          <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+            Browse our games 🎮
+          </h2>
+          <div className="w-full mb-4">
+            <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+          </div>
+          <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+            <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+              <a
+                href="#"
+                className="flex flex-wrap no-underline hover:no-underline"
+              >
+                <p className="w-full text-gray-600 text-xs md:text-sm px-6">
+                  SNAKE
+                </p>
+                <div className="w-full font-bold text-xl text-gray-800 px-6">
+                  Slither Quest 🐍
+                </div>
+                <p className="text-gray-800 text-base px-6 mb-5">
+                  Navigate the colorful world of fruits and veggies as a hungry
+                  snake! Grow longer with each nutritious bite, but watch out
+                  for the tempting treats that could shrink you down.
+                </p>
+              </a>
+            </div>
+            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+              <div className="flex items-center justify-start">
+                <button className="mx-auto lg:mx-0 hover:underline gradient text-black font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                  PLAY SNAKE
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+            <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+              <a
+                href="#"
+                className="flex flex-wrap no-underline hover:no-underline"
+              >
+                <p className="w-full text-gray-600 text-xs md:text-sm px-6">
+                  FALLING FOOD
+                </p>
+                <div className="w-full font-bold text-xl text-gray-800 px-6">
+                  NutriCatch: The Healthy Harvest 🕹️
+                </div>
+                <p className="text-gray-800 text-base px-6 mb-5">
+                  Dive into NutriCatch, where you snag healthy snacks for points
+                  while dodging the bad stuff. Quick reflexes and nutrition
+                  smarts make the game a tasty challenge for all ages!
+                </p>
+              </a>
+            </div>
+            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+              <div className="flex items-center justify-center">
+                <button className="mx-auto lg:mx-0 hover:underline gradient text-black font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                  PLAY FALLING FOOD
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+            <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+              <a
+                href="#"
+                className="flex flex-wrap no-underline hover:no-underline"
+              >
+                <p className="w-full text-gray-600 text-xs md:text-sm px-6">
+                  NEXT GAME
+                </p>
+                <div className="w-full font-bold text-xl text-gray-800 px-6">
+                  PLACEHOLDER TITLE.
+                </div>
+                <p className="text-gray-800 text-base px-6 mb-5">
+                  Placeholder description placeholder description placeholder
+                  description placeholder description placeholder description
+                  placeholder description.
+                </p>
+              </a>
+            </div>
+            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+              <div className="flex items-center justify-end">
+                <button className="mx-auto lg:mx-0 hover:underline gradient text-black font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                  PLAY PLACEHOLDER
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
