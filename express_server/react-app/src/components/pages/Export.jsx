ee4a909286b883510fd6b8ebc80a7c2c5f3106c0
Yzh-yyy <145213517+Yzh-yyy@users.noreply.github.com>
2024-04-21 20:35:08 +1000

    print certificate

--- express_server/react-app/src/components/pages/Export.jsx
+++ express_server/react-app/src/components/pages/Export.jsx
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from "react";
 import html2canvas from "html2canvas";
 import jsPDF from "jsPDF";
 import family from "../assets/family.jpg";
+import goodImage from "../assets/certificate_good.jpeg";  
+
 // download html2canvas and jsPDF and save the files in app/ext, or somewhere else
 // the built versions are directly consumable
 // import {html2canvas, jsPDF} from 'app/ext';
@@ -11,44 +13,46 @@ export default class Export extends Component {
     super(props);
   }
 
+  // printDocument() {
+  //   const input = document.getElementById("divToPrint");
+  //   html2canvas(input).then((canvas) => {
+  //     const imgData = canvas.toDataURL("image/png");
+  //     const pdf = new jsPDF();
+  //     pdf.addImage(imgData, "JPEG", 0, 0);
+  //     // pdf.output('dataurlnewwindow');
+  //     pdf.save("download.pdf");
+  //   });
+  // }
   printDocument() {
-    const input = document.getElementById("divToPrint");
-    html2canvas(input).then((canvas) => {
-      const imgData = canvas.toDataURL("image/png");
-      const pdf = new jsPDF();
-      pdf.addImage(imgData, "JPEG", 0, 0);
-      // pdf.output('dataurlnewwindow');
-      pdf.save("download.pdf");
+    const pdf = new jsPDF({
+      orientation: "landscape",  // 可以根据图片的宽高比选择适当的方向
     });
+  
+    // 获取图片的尺寸，根据需要调整下面的addImage参数
+    var img = new Image();
+    img.onload = () => {
+      const imgWidth = img.naturalWidth;
+      const imgHeight = img.naturalHeight;
+      const width = pdf.internal.pageSize.getWidth();
+      const height = pdf.internal.pageSize.getHeight();
+      const ratio = Math.min(width / imgWidth, height / imgHeight);
+      const newWidth = imgWidth * ratio;
+      const newHeight = imgHeight * ratio;
+  
+      pdf.addImage(goodImage, 'JPEG', 0, 0, newWidth, newHeight);
+      pdf.save("download.pdf");
+    };
+    img.src = goodImage;
   }
-
+  
   render() {
     return (
-      <div className="pt-24 bg-gradient-to-r from-cyan-300 to-blue-900">
-        <button className="text-black" onClick={this.printDocument}>
-          Print
-        </button>
-        <div
-          className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center"
-          id="divToPrint"
-        >
-          <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-            <p className="uppercase tracking-loose w-full">
-              Does your kid know what's healthy?
-            </p>
-            <h1 className="my-4 text-5xl font-bold leading-tight">
-              We can help you!
-            </h1>
-            <p className="leading-normal text-2xl mb-8">
-              Embark on a noble quest with your children to uncover the wonders
-              of healthy eating and why it matters! 🛡️🍏
-            </p>
-          </div>
-          <div className="w-full md:w-3/5 py-6 text-center">
-            <img className="w-full md:w-4/5 z-50" src={family} alt="Family" />
-          </div>
-        </div>
-      </div>
+      <div className="pt-24">
+      <button className="text-black" onClick={this.printDocument}>
+        Print PDF
+      </button>
+    </div>
+   
       // {/* <div className="p-36">
       //   <div className="mb5">
       //     <button className="text-black" onClick={this.printDocument}>
