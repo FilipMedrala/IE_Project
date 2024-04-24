a517b352966607129fe1a23a58adb2ff512f0df9
Yzh-yyy <yzhe0106@student.monash.edu>
2024-04-25 02:27:19 +1000

    fixed card conflict

--- express_server/react-app/src/components/pages/MemoryCard.jsx
+++ express_server/react-app/src/components/pages/MemoryCard.jsx
@@ -4,7 +4,7 @@ import "../pages/MemoryCard.css"
 const MemoryCard = ({card, handleCardClick, disabled}) => {
 
   return (
-    <button className={`card ${card.matchFound ? "matched" : ""}`} disabled={disabled} onClick={handleCardClick} data-id={card.id}>
+    <button className={`m-card ${card.matchFound ? "matched" : ""}`} disabled={disabled} onClick={handleCardClick} data-id={card.id}>
       <div className="front side">
         <img src={head}  width="60" />
       </div>
